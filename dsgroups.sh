ff9056ab6431e622ed76ce17dd2e76a18b7edc41
Mike Pullen <mike@thepullen.net>
2011-12-15 16:10:36 -0600

    added comments

--- dsgroups.sh
+++ dsgroups.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# gather list of all computers in OD
+
 computers=`dscl /LDAPv3/od1-sdob.ad.barabooschools.net -list Computers`
+
+# check membership in our top-level computer group, z_allcomputers.
+# machines should be assigned to lower-level groups, all of which eventually are members of z_allcomputers.
+# print computername only if not member of z_allcomputers
 
 for c in $computers
 do
@@ -9,3 +15,5 @@ do
 		then echo $gp
 	fi
 done
+
+exit 0
